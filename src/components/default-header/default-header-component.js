8f74bb42b0f21c90c30ade6c7fafc800ce90d457
Sai <telugusoftwarehub@gmail.com>
2024-01-09 07:11:47 +0530

    Modal popup with login form

--- src/components/default-header/default-header-component.js
+++ src/components/default-header/default-header-component.js
@@ -1,13 +1,15 @@
 import defaultLogo from "../../images/logo.svg";
 import Nav from "../common/nav";
 import Logo from "../common/logo";
+import Modal from "../common/modal";
 
 const DefaultHeaderComponent = (props) => {
     return <div className="flex">
         <Logo src={defaultLogo} />
         <Nav menuItems={props.menuItems} />
         <div className="btnSignIn">
-            <button>Sign In</button>
+            <button onClick={props.signInClickHandler}>Sign In</button>
+            {props.showModal && <Modal closeEventHandler={props.closeModalHandler} />}
         </div>
     </div>
 }
